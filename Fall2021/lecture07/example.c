743df2a664d38aa136e1256801bcd54e71652478
kindrt <kindrtnk@illinois.edu>
2021-09-13 16:49:53 -0500

    Lecture 7 code

---
+++ Fall2021/lecture07/example.c
@@ -0,0 +1,16 @@
+/* to complile: gcc example.c -o example
+ * to run: ./example
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int answer = 42;  // the Answer!
+
+    printf ("The answer is %d.\n", answer);
+
+    /* Our work here is done.
+    *  Let's get out of here! */
+    return 0;
+} 
